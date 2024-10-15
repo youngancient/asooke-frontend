4804fe72637ead55b4a7368854f1800252e220bc
youngancient <tochyokoye@gmail.com>
2024-10-16 00:15:03 +0100

    fix conflicts

--- src/components/sections/AfterHero.tsx
+++ src/components/sections/AfterHero.tsx
@@ -5,9 +5,13 @@ export const AfterHero = () => {
     <section className="py-20 px-5 w-full bg-[#56BD61] flex items-center justify-center">
       <div className="w-[95%] flex justify-between items-center md:flex-row flex-col">
         <div className="md:w-[30%]">
-          <h3 className="font-bold text-4xl">Discover Unique Fashion Experiences</h3>
+          <h3 className="font-bold text-4xl">
+            Discover Unique Fashion Experiences
+          </h3>
           <p className="mt-10 text-2xl">
-            Connect with local artisans and global designers to explore a curated collection of traditional and contemporary fabrics, ensuring every piece tells a story.
+            Connect with local artisans and global designers to explore a
+            curated collection of traditional and contemporary fabrics, ensuring
+            every piece tells a story.
           </p>
 
           <div className="mt-5">
@@ -22,18 +26,30 @@ export const AfterHero = () => {
 
         <div className="md:w-[70%] flex md:flex-row flex-col px-5 mt-10 md:mt-0">
           <div className="md:w-[50%]">
-            <img className="w-[48px]" src="/assets/icon2.png" alt="Traditional Fabrics" />
+            <img
+              className="w-[48px]"
+              src="/assets/icon2.png"
+              alt="Traditional Fabrics"
+            />
             <h3 className="font-bold text-2xl">Explore Traditional Fabrics</h3>
             <p className="mt-10 text-xl">
-              Dive into a diverse selection of authentic fabrics like Aso Oke and Ankara, sourced directly from local sellers. Each piece supports artisans and preserves cultural heritage.
+              Dive into a diverse selection of authentic fabrics like Aso Oke
+              and Ankara, sourced directly from local sellers. Each piece
+              supports artisans and preserves cultural heritage.
             </p>
           </div>
 
           <div className="md:w-[50%] mt-5 md:mt-0">
-            <img className="w-[48px]" src="/assets/icon2.png" alt="Custom Designs" />
+            <img
+              className="w-[48px]"
+              src="/assets/icon2.png"
+              alt="Custom Designs"
+            />
             <h3 className="font-bold text-2xl">Custom Designs Just for You</h3>
             <p className="mt-10 text-xl">
-              Work with talented designers to create bespoke outfits tailored to your personal style and occasion, ensuring a perfect fit and unique look.
+              Work with talented designers to create bespoke outfits tailored to
+              your personal style and occasion, ensuring a perfect fit and
+              unique look.
             </p>
           </div>
         </div>
