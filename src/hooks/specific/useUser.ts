9c6acb296f493b8e847ea63ecb2e0aa5112a38e9
youngancient <tochyokoye@gmail.com>
2024-10-15 12:41:42 +0100

    added registration hook

--- src/hooks/specific/useUser.ts
+++ src/hooks/specific/useUser.ts
@@ -1,11 +1,13 @@
 import { useCallback, useEffect, useState } from "react";
 import { useMarketPlaceContract } from "../useContracts";
-import { useAppKitAccount } from "@reown/appkit/react";
+import { useAppKitAccount, useAppKitNetwork } from "@reown/appkit/react";
+import { liskSepoliaNetwork } from "../../connection";
+import { toast } from "react-toastify";
 
-interface IUser{
-  displayName : string;
-  roleType : number;
-  isRegistered : boolean;
+interface IUser {
+  displayName: string;
+  roleType: number;
+  isRegistered: boolean;
 }
 export const useUser = () => {
   const [user, setUser] = useState<IUser | null>(null);
@@ -21,10 +23,60 @@ export const useUser = () => {
       console.log(error);
     }
   }, [readOnlyMarketPlaceContract]);
-  
+
   useEffect(() => {
     fetchUser();
   }, [fetchUser]);
 
   return { user };
+};
+
+export const useRegister = () => {
+  const { address } = useAppKitAccount();
+  const { chainId } = useAppKitNetwork();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const marketPlaceContract = useMarketPlaceContract(true);
+  // const errorDecoder = ErrorDecoder.create()
+  const registerUser = useCallback(
+    async (displayName: string, roleType: Number) => {
+      if (!marketPlaceContract) {
+        toast.error("Contract not found");
+        return;
+      }
+      if (!address) {
+        toast.error("Connect your wallet!");
+        return;
+      }
+      if (Number(chainId) !== liskSepoliaNetwork.chainId) {
+        toast.error("You are not connected to the right network");
+        return;
+      }
+      try {
+        setIsLoading(true);
+        const estimatedGas = await marketPlaceContract.registerUser.estimateGas(
+          { displayName, roleType }
+        );
+        // construct transaction
+        const tx = await marketPlaceContract.registerUser(
+          { displayName, roleType },
+          {
+            gasLimit: (estimatedGas * BigInt(120)) / BigInt(100),
+          }
+        );
+        const reciept = await tx.wait();
+        if (reciept.status === 1) {
+          toast.success("User Registration successful");
+          return;
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [marketPlaceContract, address]
+  );
+  return { registerUser, isLoading };
 };
