7b6cf1b15c605a4a19c33ab3735ddec5d8b93508
Emase <corneliuslochipi@gmail.com>
2024-10-15 22:55:36 +0300

    added designer router

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import Dashboard from "../pages/dashboard/Dashboard";
+import Sellers from "../pages/dashboard/sellers/Sellers";
+import Designers from "../pages/dashboard/designers/Designers";
 
 const AppRoutes = () => {
   return (
@@ -9,6 +11,8 @@ const AppRoutes = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={"Not Found"} />
+        <Route path="/sellers" element={<Sellers />} />
+        <Route path="/designers" element={<Designers />} />
       </Routes>
     </Router>
   );
